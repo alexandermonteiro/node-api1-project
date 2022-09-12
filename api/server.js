1fa300751ad2ede058d490bc8067e14af7d0fe28
Alexander Monteiro <alexandermonteiro@Priscilas-MBP.attlocal.net>
2022-09-12 16:31:10 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,7 +1,10 @@
-// BUILD YOUR SERVER HERE
 const express = require("express");
 
 const server = express();
+
+server.get("/api/users", (req, res) => {
+  res.json("users");
+});
 
 server.use("*", (req, res) => {
   res.status(404).json({
