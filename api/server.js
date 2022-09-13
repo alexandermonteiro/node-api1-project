a1f1527f1701f64d007d9200b5e02239b948efb6
Alexander Monteiro <alexandermonteiro@Priscilas-MBP.attlocal.net>
2022-09-12 19:22:29 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -5,6 +5,32 @@ const server = express();
 
 server.use(express.json());
 
+server.put("/api/users/:id", async (req, res) => {
+  try {
+    const possibleUser = await User.findById(req.params.id);
+    if (!possibleUser) {
+      res.status(404).json({
+        message: "The user with the specified ID does not exist found",
+      });
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: "Please provide name and bio for the user",
+        });
+      } else {
+        const updateUser = await User.update(req.params.id, req.body);
+        res.status(200).json(updateUser);
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "error updating user",
+      err: err.message,
+      stack: err.stack,
+    });
+  }
+});
+
 server.delete("/api/users/:id", async (req, res) => {
   try {
     const possibleUser = await User.findById(req.params.id);
